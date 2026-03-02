1527aa29e63039d79a0497f9045590bdae6f599b
Eliasmalx <eliasmal0228@gmail.com>
2026-03-02 15:25:35 +0100

    Mejorando app.py para no recibir warning y al abrir el puerto desde el back no  de un Not Found

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from database.dbHelpDesk import db
-from src.auth.routes import auth_bp
+from src.auth.routes import auth_bp, bcrypt
 from src.tickets.routes import tickets_bp
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
@@ -22,9 +22,11 @@ db_host = os.getenv('DB_HOST', 'localhost')
 db_name = os.getenv('DB_NAME', 'helpdesk_db')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{quote_plus(db_password)}@{db_host}/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'tu_clave_secreta_super_segura_cambiar_en_produccion')
 
 db.init_app(app)
+bcrypt.init_app(app)
 jwt = JWTManager(app)
 app.register_blueprint(auth_bp)
 app.register_blueprint(tickets_bp)
@@ -32,5 +34,9 @@ app.register_blueprint(tickets_bp)
 with app.app_context():
     db.create_all()
 
+@app.route('/')
+def home():
+    return '¡Bienvenido al sistema de Help Desk!'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host='127.0.0.1')
